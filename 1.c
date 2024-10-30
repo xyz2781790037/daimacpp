f54435a0ba2d5cea3853e79d7a16c3ce4e95bffd
xyz2781790037 <2781790037@qq.com>
2024-10-30 18:21:54 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,13 @@
-#include<iostream>
+//7.编写一个程序，提示用户输入一个double类型的数，并打印该数的立方值。自己设计一个函数计算并打印立方值。main() 函数要把用户输入的值传递给该函数。
+#include<stdio.h>
+void cube(double n)
+{
+    printf("%.6lf", n * n * n);
+}
+int main()
+{
+    double n;
+    scanf("%lf", &n);
+    cube(n);
+    return 0;
+}
