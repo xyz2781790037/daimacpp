a98c0575b3547a7fee071bfca318050176f759fd
xyz2781790037 <2781790037@qq.com>
2024-11-07 21:57:12 +0800

    1

--- DataStructres/seqlist/Seqlist.h
+++ DataStructres/seqlist/Seqlist.h
@@ -27,7 +27,15 @@ typedef struct Seqlist // 静态顺序表
 void SeqlistInit(sl *ps);
 void SeqlistPushBack(sl *ps, Sldatatype x);
 void SeqlistPopBack(sl *ps);
+void SeqlistCheckCapacity(sl *ps);
 void SeqlistPushFront(sl *ps, Sldatatype x);
 void SeqlistPopFront(sl *ps);
 void ceshi(sl *ps);
 void SeqlistDestory(sl *ps);
+
+//找到了返回x的下标位置插入，没有返回-1
+int SeqlistFind(sl *ps, Sldatatype x);
+//指定pos下标位置插入
+void SeqlistInsert(sl *ps, int pos,Sldatatype x);
+//删除pos位置的数据
+void SeqlistErase(sl *ps, int pos);
