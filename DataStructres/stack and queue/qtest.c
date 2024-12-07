1ab89d4ad4741b7f65f60d9e8b668f523246cac7
xyz2781790037 <2781790037@qq.com>
2024-12-07 20:42:35 +0800

    file

--- DataStructres/stack and queue/qtest.c
+++ DataStructres/stack and queue/qtest.c
@@ -1,1 +1,26 @@
 #include "queue.h"
+void TestQueue()
+{
+    Queue q;
+    QueueInit(&q);
+    QueuePush(&q, 1);
+    QueuePush(&q, 2);
+    QueuePush(&q, 3);
+    QueuePush(&q, 4);
+
+    QueuePop(&q);
+    QueuePop(&q);
+    QueuePop(&q);
+    QueuePop(&q);
+    QueuePush(&q, 10);
+    QueuePush(&q, 20);
+
+    printf("%d\n", QueueBack(&q));
+    QueueDestroy(&q);
+}
+int main()
+{
+    int a;
+    
+    return 0;
+}
--- DataStructres/stack and queue/queue.c
+++ DataStructres/stack and queue/queue.c
@@ -5,11 +5,61 @@ void QueueInit(Queue *pq)
     pq->head = NULL;
     pq->tail = NULL;
 }
-void QueueInit(Queue *pq);
-void QueueDestroy(Queue *pq);
-void QueuePush(Queue *pq, qdatatype x);
-void QueuePop(Queue *pq);
-qdatatype QueueFront(Queue *pq);
-qdatatype QueueBack(Queue *pq);
+void QueueDestroy(Queue *pq)
+{
+    assert(pq);
+    QNode *cur = pq->head;
+    while(cur != NULL)
+    {
+        QNode *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    pq->head = pq->tail = NULL;
+}
+void QueuePush(Queue *pq, qdatatype x)
+{
+    assert(pq);
+    QNode *newnode = (QNode *)malloc(sizeof(QNode));
+    newnode->data = x;
+    newnode->next = NULL;
+    if (pq->head == NULL)
+    {
+        pq->head = pq->tail = newnode;
+    }
+    else
+    {
+        pq->tail->next = newnode;
+        pq->tail = newnode;
+    }
+}
+void QueuePop(Queue *pq)
+{
+    assert(pq);
+    assert(!QueueEmpty(pq));
+    QNode *next = pq->head->next;
+    free(pq->head);
+    pq->head = next;
+    if(pq->head == NULL)
+    {
+        pq->tail == NULL;
+    }
+}
+qdatatype QueueFront(Queue *pq)
+{
+    assert(pq);
+    assert(!QueueEmpty(pq));
+    return pq->head->data;
+}
+qdatatype QueueBack(Queue *pq)
+{
+    assert(pq);
+    assert(!QueueEmpty(pq));
+    return pq->tail->data;
+}
 int QueueSize(Queue *pq);
-bool QueueEmpty(Queue *pq);
+bool QueueEmpty(Queue *pq)
+{
+    assert(pq);
+    return pq->head == NULL;
+}
