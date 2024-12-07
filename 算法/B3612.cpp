560351976ae3a3640711a9b55078b4cc47696999
xyz2781790037 <2781790037@qq.com>
2024-12-07 18:26:13 +0800

    file

--- 算法/B3612.cpp
+++ 算法/B3612.cpp
@@ -3,27 +3,37 @@ using namespace std;
 class Solution
 {
 public:
-    bool isPalindrome(string s, int x = 0)
+    bool isPalindrome(string s,int left = 0,int right = 0)
     {
-        char *ss = (char *)malloc(s.size());
-        char *sss = (char *)malloc(s.size());
+        string ss;
+
         for (int i = 0; i < s.size(); i++)
         {
             if (isalnum(s[i]))
             {
                 if (isupper(s[i]))
                 {
-                    ss[x++] = tolower(s[i]);
+                    ss += tolower(s[i]);
                 }
                 else
                 {
-                    ss[x++] = s[i];
+                    ss += s[i];
                 }
             }
         }
-        for (int i = strlen(ss) - 1; i >= 0; i--)
+        right = ss.size() - 1;
+        while (left < right)
         {
-
+            if(ss[left] != ss[right])
+            {
+                return false;
+            }
         }
+        return true;
     }
 };
+int main(){
+    string s = "A man, a plan, a canal: Panama";
+    Solution p;
+    p.isPalindrome(s);
+}
